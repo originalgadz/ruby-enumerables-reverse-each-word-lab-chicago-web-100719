8da4ff130ff7db9bb80fdecaedb98dd147a12ed3
Glenn A Doherty <gadz@earthlink.net>
2019-08-24 23:43:25 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,10 @@
+def reverse_each_word(sentence)
+  rev_sentence = []
+  sentence.split.each do |word|
+    rev_sentence.push (word.reverse)
+  end
+  rev_sentence.join(" ")
+end
 
 def reverse_each_word(sentence)
   rev_sentence=sentence.split
